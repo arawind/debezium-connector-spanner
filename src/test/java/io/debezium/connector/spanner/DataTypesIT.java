cc003e733794ba5518620802389e410fcce956d3
Aravind <aravindp1510@gmail.com>
2024-07-18 09:44:31 +0530

    DBZ-8043 Update DataTypesIT to work with PG dialect

--- src/test/java/io/debezium/connector/spanner/DataTypesIT.java
+++ src/test/java/io/debezium/connector/spanner/DataTypesIT.java
@@ -10,26 +10,35 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.google.cloud.spanner.Dialect;
 
 import io.debezium.config.Configuration;
+import io.debezium.connector.spanner.util.Connection;
+import io.debezium.connector.spanner.util.Database;
 
 public class DataTypesIT extends AbstractSpannerConnectorIT {
 
+    private static final Database pgDatabase = Database.builder()
+            .generateDatabaseId()
+            .dialect(Dialect.POSTGRESQL)
+            .build();
+    private static final Connection pgDatabaseConnection = pgDatabase.getConnection();
+
     private static final String tableName = "embedded_data_types_tests_table";
     private static final String changeStreamName = "embeddedDataTypesTestChangeStream";
-    private static final List<String> types = Arrays.asList("BOOL", "INT64",
-            "FLOAT32", "FLOAT64", "TIMESTAMP", "DATE", "STRING", "BYTES",
-            "NUMERIC", "JSON");
 
     @BeforeAll
     static void setup() throws InterruptedException, ExecutionException {
@@ -49,6 +58,22 @@ public class DataTypesIT extends AbstractSpannerConnectorIT {
                 + ") PRIMARY KEY (id)");
         databaseConnection.createChangeStream(changeStreamName, tableName);
 
+        pgDatabaseConnection.createTable(tableName + "(id bigint,"
+                + "  boolCol boolean,"
+                + "  int64Col bigint,"
+                + "  float32Col float4,"
+                + "  float64Col float8,"
+                + "  timestampCol timestamptz,"
+                + "  dateCol date,"
+                + "  stringCol varchar,"
+                + "  bytesCol bytea,"
+                + "  numericCol numeric,"
+                + "  jsonCol jsonb,"
+                + "  arrCol varchar[],"
+                + "  PRIMARY KEY (id)"
+                + ")");
+        pgDatabaseConnection.createChangeStream(changeStreamName, tableName);
+
         System.out.println("DataTypesIT is ready...");
     }
 
@@ -56,26 +81,39 @@ public class DataTypesIT extends AbstractSpannerConnectorIT {
     static void clear() throws InterruptedException {
         databaseConnection.dropChangeStream(changeStreamName);
         databaseConnection.dropTable(tableName);
+        pgDatabaseConnection.dropChangeStream(changeStreamName);
+        pgDatabaseConnection.dropTable(tableName);
     }
 
-    @Test
-    public void shouldStreamUpdatesToKafkaWithTheCorrectType() throws InterruptedException {
+    private static Stream<Arguments> dialectProvider() {
+        return Stream.of(
+                Arguments.of(database, databaseConnection, "(1, true, 42, 3.14, 2.71, '1970-01-01 00:00:00 UTC',"
+                        + " '1970-01-01', 'stringVal', b'bytesVal', 6.023,"
+                        + " JSON '\"Hello\"', ['a', 'b'])"),
+                Arguments.of(pgDatabase, pgDatabaseConnection, "(2, true, 42, 3.14, 2.71, '1970-01-01 00:00:00 UTC',"
+                        + " '1970-01-01', 'stringVal', bytea 'bytesVal', 6.023,"
+                        + " JSONB '\"Hello\"', ARRAY['a', 'b'])"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("dialectProvider")
+    public void shouldStreamUpdatesToKafkaWithTheCorrectType(Database db, Connection dbConnection, String valueList)
+            throws InterruptedException, ExecutionException {
         final Configuration config = Configuration.copy(baseConfig)
+                .with("gcp.spanner.database.id", db.getDatabaseId())
                 .with("gcp.spanner.change.stream", changeStreamName)
                 .with("name", tableName + "_test")
                 .with("gcp.spanner.start.time",
                         DateTimeFormatter.ISO_INSTANT.format(Instant.now()))
                 .build();
+
         initializeConnectorTestFramework();
         start(SpannerConnector.class, config);
         assertConnectorIsRunning();
-        databaseConnection.executeUpdate("INSERT INTO " + tableName
+        dbConnection.executeUpdate("INSERT INTO " + tableName
                 + "(id, boolCol, int64Col, float32Col, float64Col, timestampCol,"
                 + " dateCol, stringCol, bytesCol, numericCol, jsonCol, arrCol) "
-                + "VALUES"
-                + " (1, true, 42, 3.14, 2.71, '1970-01-01 00:00:00 UTC',"
-                + "  '1970-01-01', 'stringVal', b'bytesVal', 6.023,"
-                + "  JSON '\"Hello\"', ['a', 'b'])");
+                + "VALUES " + valueList);
         waitForAvailableRecords(waitTimeForRecords(), TimeUnit.SECONDS);
         SourceRecords sourceRecords = consumeRecordsByTopic(10, false);
         List<SourceRecord> records = sourceRecords.recordsForTopic(getTopicName(config, tableName));
